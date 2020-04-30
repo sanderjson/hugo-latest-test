9ecfaec54009e966c43f03347c6956fe713e6293
sanderjson <sanderson.jma@gmail.com>
2020-04-29 18:58:09 -0600

    test writeStats

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,5 +1,5 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [ './hugo_stats.json' ],
+  content: [ 'src/hugo_stats.json' ],
   defaultExtractor: (content) => {
       let els = JSON.parse(content).htmlElements;
       return els.tags.concat(els.classes, els.ids);
